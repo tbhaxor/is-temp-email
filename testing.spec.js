f4d3336dd7abfbaeffc4bdddb0fb98d1e3346dc0
Gurkirat Singh <tbhaxor@gmail.com>
2021-08-18 04:33:09 +0530

    add more test for single email check

--- testing.spec.js
+++ testing.spec.js
@@ -24,10 +24,31 @@ describe('Temporary email checker', function () {
       .catch(done)
   })
 
+  it("should not detect protonmail.com temporary", (done) => {
+    isTempEmail
+      .single("tbhaxor@protonmail.com")
+      .then((r) => {
+        expect(r).to.be.false
+        done()
+      })
+      .catch(done)
+  })
+
+  it("should not detect gmail.com temporary", (done) => {
+    isTempEmail
+      .single("tbhaxor@gmail.com")
+      .then((r) => {
+        expect(r).to.be.false
+        done()
+      })
+      .catch(done)
+  })
+
   it('should do bulk detecting', (done) => {
     const emails = {
       'tbhaxor@yopmail.com': true,
       'tbhaxor@gmail.com': false,
+      'tbhaxor@protonmail.com': false,
       'relor60727@zefara.com': true,
     }
     isTempEmail
